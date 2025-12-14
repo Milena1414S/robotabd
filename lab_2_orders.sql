5e49bd17387701f2906de0dd360b24d1876cf37d
Milena Khryplyvets <Milena.Khryplyvets@hneu.net>
2025-12-14 17:37:56 +0200

    Commit 2

---
+++ lab_2_orders.sql
@@ -0,0 +1,11 @@
+INSERT INTO `Orders` (`user_id`, `status`) VALUES
+(3, 'completed'),  
+(2, 'shipped'),    
+(1, 'pending'),    
+(4, 'completed'),  
+(6, 'pending'),    
+(8, 'shipped'),    
+(8, 'completed'),
+(2, 'cancelled'),  
+(1, 'shipped'),    
+(3, 'completed'); 
